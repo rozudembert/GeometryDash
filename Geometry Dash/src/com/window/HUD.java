c44a59e27dab4a5bfaae17d44578c592cc8d286e
kelmr <kelmr@DESKTOP-9H3N8JL.fritz.box>
2021-03-07 16:47:06 +0100

    Documentation

--- Geometry Dash/src/com/window/HUD.java
+++ Geometry Dash/src/com/window/HUD.java
@@ -7,6 +7,7 @@ import com.framework.GameObject;
 
 public class HUD {
 	
+	//the maximum lenght of a level is 1024 blocks.
 	public static float LEVEL_LENGTH = 1024 * 64;
 	
 	Camera camera;
@@ -19,20 +20,28 @@ public class HUD {
 	private float progress = 0;
 	
 	public void update(GameObject player) {
-		progress = (player.getX() / LEVEL_LENGTH) * 100;
+		progress = (player.getX() / LEVEL_LENGTH) * 600;
 		System.out.println(progress);
 	}
 	
 	public void render(Graphics g) {
+		int boxWidth = 600;
+		int boxHeight = 20;
+		int x_position = -(int)camera.getX() + Game.WIDTH/2 - boxWidth/2;
+		int y_position = -(int)camera.getY()+ 10;
 		
-		g.setColor(Color.GRAY);
-		g.fillRect(-(int)camera.getX() + Game.WIDTH/2 - 300, -(int)camera.getY()+ 10, 600, 30);
 		
+		//progress bar background
+		//g.setColor(Color.GRAY);
+		//g.fillRect(x_position, y_position, boxWidth, boxHeight);
+		
+		//progess bar
 		g.setColor(Color.GREEN);
-		g.fillRect(-(int)camera.getX() + Game.WIDTH/2 - 300, -(int)camera.getY()+ 10, (int)progress*6, 30);
+		g.fillRect(x_position, y_position, (int)progress, boxHeight);
 		
+		//White outline
 		g.setColor(Color.WHITE);
-		g.drawRect(-(int)camera.getX() + Game.WIDTH/2 - 300, -(int)camera.getY()+ 10, 600, 30);
+		g.drawRect(x_position, y_position, boxWidth, boxHeight);
 		
 		
 	}
