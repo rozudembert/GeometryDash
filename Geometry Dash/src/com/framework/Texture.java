91ce5c92c4709f7f1bfe80a4b3323b82c2b250ef
kelmr <kelmr@DESKTOP-9H3N8JL.fritz.box>
2021-03-07 15:07:00 +0100

    new Textures

--- Geometry Dash/src/com/framework/Texture.java
+++ Geometry Dash/src/com/framework/Texture.java
@@ -21,7 +21,7 @@ public class Texture {
 	private BufferedImage button_sheet = null;
 	
 	//array to store the images in
-	public BufferedImage[] block = new BufferedImage[23]; //array has to have the size of the amount of blocks
+	public BufferedImage[] block = new BufferedImage[32]; //array has to have the size of the amount of blocks
 	public BufferedImage[] player = new BufferedImage[8];
 	public BufferedImage[] button = new BufferedImage[21];
 	
@@ -56,28 +56,37 @@ public class Texture {
 	//assign the images from the sheets to the arrays
 	public void getTextures() {
 		block[0] = bs.grabImage(1, 1, 64, 64);	//Green Block
-		block[1] = bs.grabImage(2, 1, 64, 64);	//Andesite
-		block[2] = bs.grabImage(3, 1, 64, 64);	//Stone
-		block[3] = bs.grabImage(4, 1, 64, 64);	//Obsidian
-		block[4] = bs.grabImage(5, 1, 64, 64);	//Bedrock
-		block[5] = bs.grabImage(6, 1, 64, 64);	//Stone
-		block[6] = bs.grabImage(7, 1, 64, 64);	//Stone
-		block[7] = bs.grabImage(8, 1, 64, 64);	//Stone
+		block[1] = bs.grabImage(2, 1, 64, 64);	//Red-Black-Gradient
+		block[2] = bs.grabImage(3, 1, 64, 64);	//Blue Cement
+		block[3] = bs.grabImage(4, 1, 64, 64);	//Magenta Cement
+		block[4] = bs.grabImage(5, 1, 64, 64);	//Lime Cement
+		block[5] = bs.grabImage(6, 1, 64, 64);	//Yellow Cement
+		block[6] = bs.grabImage(7, 1, 64, 64);	//Water
+		block[7] = bs.grabImage(8, 1, 64, 64);	//Top Water
 		block[8] = bs.grabImage(1, 2, 64, 64);	//Grass
 		block[9] = bs.grabImage(2, 2, 64, 64);	//Dirt
-		block[10] = bs.grabImage(3, 2, 64, 64);	//Grass
-		block[11] = bs.grabImage(4, 2, 64, 64);	//Wood
-		block[12] = bs.grabImage(5, 2, 64, 64);	//Wood
-		block[13] = bs.grabImage(6, 2, 64, 64);	//Log
+		block[10] = bs.grabImage(3, 2, 64, 64);	//Plank
+		block[11] = bs.grabImage(4, 2, 64, 64);	//Plank
+		block[12] = bs.grabImage(5, 2, 64, 64);	//Oak Log
+		block[13] = bs.grabImage(6, 2, 64, 64);	//Birch Log
 		block[14] = bs.grabImage(7, 2, 64, 64);	//Leaves
-		block[15] = bs.grabImage(8, 2, 64, 64);	//Endstone
+		block[15] = bs.grabImage(8, 2, 64, 64);	//leaves
 		block[16] = bs.grabImage(1, 3, 64, 64);	//Cobblestone
 		block[17] = bs.grabImage(2, 3, 64, 64);	//Stone
 		block[18] = bs.grabImage(3, 3, 64, 64);	//Diamonds
 		block[19] = bs.grabImage(4, 3, 64, 64);	//Emeralds
 		block[20] = bs.grabImage(5, 3, 64, 64);	//Iron
-		block[21] = bs.grabImage(6, 3, 64, 64);	//Leaves
-		block[22] = bs.grabImage(7, 3, 64, 64);	//Transparent Block
+		block[21] = bs.grabImage(6, 3, 64, 64);	//Gold
+		block[22] = bs.grabImage(7, 3, 64, 64);	//Stone Brick
+		block[23] = bs.grabImage(8, 3, 64, 64); //Mossy Stone Brick
+		block[24] = bs.grabImage(1, 4, 64, 64); //Netherack
+		block[25] = bs.grabImage(2, 4, 64, 64); //Obsidian
+		block[26] = bs.grabImage(3, 4, 64, 64); //Nether Brick
+		block[27] = bs.grabImage(4, 4, 64, 64);	//Lapis 
+		block[28] = bs.grabImage(5, 4, 64, 64); //Gravel
+		block[29] = bs.grabImage(6, 4, 64, 64);	//Magma
+		block[30] = bs.grabImage(7, 4, 64, 64); //Lava
+		block[31] = bs.grabImage(8, 4, 64, 64); //Lava Top
 		
 		//Player Skin
 		player[0] = ps.grabImage(1, 1, 64, 64);
--- Geometry Dash/src/com/window/Controller.java
+++ Geometry Dash/src/com/window/Controller.java
@@ -107,33 +107,43 @@ public class Controller {
 	
 	//add block depending on rgb color
 	public void loadBlocks(int r, int g, int b, int xx, int yy) {
-
+		int type = 100;
 		
-		if(r == 255 && g == 255 && b == 255) addObject(new Block(xx*64, yy*64, 0, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 1, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 2, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 3, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 4, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 5, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 6, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 7, ObjectId.Block));
-		else if(r == 0 && g == 110 && b == 47) addObject(new Block(xx*64, yy*64, 8, ObjectId.Block));
-		else if(r == 163 && g == 72 && b == 47) addObject(new Block(xx*64, yy*64, 9, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 10, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 11, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 12, ObjectId.Block));
-		else if(r == 132 && g == 64 && b == 64) addObject(new Block(xx*64, yy*64, 13, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 14, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 15, ObjectId.Block));
-		else if(r == 64 && g == 64 && b == 64) addObject(new Block(xx*64, yy*64, 16, ObjectId.Block));
-		else if(r == 128 && g == 128 && b == 128) addObject(new Block(xx*64, yy*64, 17, ObjectId.Block));
-		else if(r == 0 && g == 255 && b == 255) addObject(new Block(xx*64, yy*64, 18, ObjectId.Block));
-		else if(r == 0 && g == 255 && b == 0) addObject(new Block(xx*64, yy*64, 19, ObjectId.Block));
-		else if(r == 255 && g == 255 && b == 0) addObject(new Block(xx*64, yy*64, 20, ObjectId.Block));
-		else if(r == 0 && g == 31 && b == 0) addObject(new Block(xx*64, yy*64, 21, ObjectId.Block));
-		else if(r == 255 && g == 0 && b == 0) addObject(new Block(xx*64, yy*64, 22, ObjectId.Block));
-		//else if(r == ? && g == ? && b == ?) addObject(new Block(xx*64, yy*64, 23, ObjectId.Block));
+		if(r == 255 && g == 255 && b == 255) type = 0; //Green Block
+		else if(r == 99 && g == 19 && b == 19) type = 1; //Red-Black-Gradient
+		else if(r == 37 && g == 138 && b == 200) type = 2; //Blue Cement
+		else if(r == 169 && g == 48 && b == 159) type = 3; //Magenta Cement
+		else if(r == 95 && g == 170 && b == 25) type = 4; //Lime Cement
+		else if(r == 239 && g == 174 && b == 21) type = 5; //Yellow Cement
+		else if(r == 99 && g == 127 && b == 175) type = 6; //Water
+		else if(r == 117 && g == 172 && b == 219) type = 7; //Top Water
+		else if(r == 0 && g == 110 && b == 47) type = 8; //Grass
+		else if(r == 163 && g == 72 && b == 47) type = 9; //Dirt
+		else if(r == 112 && g == 89 && b == 54) type = 10; //Plank
+		else if(r == 58 && g == 46 && b == 29) type = 11; //Plank
+		else if(r == 132 && g == 64 && b == 64) type = 12; //Oak Log
+		else if(r == 179 && g == 183 && b == 169) type = 13; //Birch Log
+		else if(r == 0 && g == 31 && b == 0) type = 14; //Leaves
+		else if(r == 104 && g == 135 && b == 50) type = 15; //leaves
+		else if(r == 64 && g == 64 && b == 64) type = 16; //Cobblestone
+		else if(r == 128 && g == 128 && b == 128) type = 17; //Stone
+		else if(r == 0 && g == 255 && b == 255) type = 18; //Diamonds
+		else if(r == 0 && g == 255 && b == 0) type = 19; //Emeralds
+		else if(r == 205 && g == 174 && b == 128) type = 20; //Iron
+		else if(r == 255 && g == 255 && b == 0) type = 21; //Gold
+		else if(r == 137 && g == 137 && b == 137) type = 22; //Stone Brick
+		else if(r == 69 && g == 81 && b == 57) type = 23; //Mossy Stone Brick
+		else if(r == 124 && g == 32 && b == 32) type = 24; //Netherack
+		else if(r == 65 && g == 56 && b == 90) type = 25; //Obsidian
+		else if(r == 56 && g == 26 && b == 31) type = 26; //Nether Brick
+		else if(r == 48 && g == 64 && b == 190) type = 27; //Lapis 
+		else if(r == 176 && g == 158 && b == 158) type = 28; //Gravel
+		else if(r == 255 && g == 156 && b == 0) type = 29; //Magma
+		else if(r == 215 && g == 123 && b == 45) type = 30; //Lava
+		else if(r == 224 && g == 154 && b == 56) type = 31; //Lava Top
 		
+		if(type != 100)
+			addObject(new Block(xx*64, yy*64, type, ObjectId.Block));
 	}
 	
 	
